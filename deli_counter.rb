f12e574f5087d27b73a9dd2e3903cc59550e1631
Caleb Einarr <earthctzn1@gmail.com>
2019-04-07 17:37:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(array)
   if array = []
     puts "The line is currently empty."
     elsif array =! []
-    puts "The line is currently #{array(index)}"
+    puts "The line is currently #{array}"
   end
 end
